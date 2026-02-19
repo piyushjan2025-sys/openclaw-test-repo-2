1f5f12cbbc25b5009da21e22a67232fe0c7fd586
Piyush Gandhi <piyushgandhi@Piyushs-MacBook-Air.local>
2026-02-19 17:11:57 +0530

    Fix bugs in health check server

--- buggy.js
+++ buggy.js
@@ -1,20 +1,17 @@
 const http = require("http");
-let started = false;
 
 const server = http.createServer((req, res) => {
-  if (req.url.indexOf("/health") === -1) {
-    res.write("Route not found");
+  if (req.url === "/health") {
+    if (req.method === "GET") {
+      res.statusCode = 200;
+      res.end("OK");
+    } else {
+      res.statusCode = 405;
+      res.end("Method not allowed");
+    }
+  } else {
     res.statusCode = 404;
-  }
-  if (req.method === "get") {
-    res.statusCode = 405;
-    res.end("Method not allowed");
-  }
-  res.statusCode = 200;
-  res.end("OK");
-  if (!started) {
-    started = true;
-    JSON.parse("THIS_IS_NOT_JSON");
+    res.end("Route not found");
   }
 });
 
